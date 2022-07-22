5a0df26c009d07d137e409fedcf737f29a8a36de
Vasil Pashov <vasil.pashov1@gmail.com>
2022-07-22 16:17:33 +0300

    Add Sorting/Seating from informatika.bg, sadly it gets only 60/100

---
+++ Competitive/Sorting/Seating/seating.cpp
@@ -0,0 +1,77 @@
+// Link to task: https://action.informatika.bg/problems/128
+// 
+// TODO: It seems like the task requires to count the inversions
+// in the array. This is done via merge sort, but the solution gets
+// only 60 out of 100 points. (I've tried also to copy paste various
+// inversion counters from the Internet but still get the same 60/100
+// result).
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cassert>
+#include <cctype>
+
+using namespace std;
+
+int64_t merge(
+	std::vector<int>& source,
+	std::vector<int>& dest,
+	int start,
+	int end,
+	int mid
+) {
+	int64_t inversions = 0;
+	int left = start;
+	int right = mid + 1;
+	int k = start;
+	while (left <= mid && right <= end) {
+		if (source[left] <= source[right]) {
+			dest[k++] = source[left++];
+		} else {
+			dest[k++] = source[right++];
+			// Every time something from the right part has to be taken in the main
+			// array count how many elements did it "skip over" (i.e. the number of
+			// unprocessed elements from the left side)
+			inversions += (mid + 1 - left);
+		}
+	}
+	while (left <= mid) dest[k++] = source[left++];
+	while (right <= end) dest[k++] = source[right++];
+	return inversions;
+}
+
+int64_t mergeSortHelper(
+	std::vector<int>& source,
+	std::vector<int>& dest,
+	int start,
+	int end
+) {
+	if (start == end) {
+		return 0;
+	}
+	const int mid = (start + end) / 2;
+	int64_t inversions = mergeSortHelper(dest, source, start, mid);
+	inversions += mergeSortHelper(dest, source, mid + 1, end);
+	inversions += merge(source, dest, start, end, mid);
+	return inversions;
+}
+
+int64_t mergeSort(std::vector<int>& seatings) {
+	std::vector<int> helper = seatings;
+	return mergeSortHelper(helper, seatings, 0, seatings.size() - 1);
+}
+
+int main() {
+	int n;
+	cin >> n;
+	std::vector<int> seatings(n);
+	for (int i = 0; i < n; ++i) {
+		int el;
+		scanf("%d", &el);
+		seatings[i] = el;
+	}
+	int64_t swapCount = mergeSort(seatings);
+	cout << swapCount << '\n';
+	return 0;
+}
